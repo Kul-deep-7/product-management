b464e2ce0e405ac38fabea4a71d73c4cf7a57319
Kul-deep-7 <sahiljangid321@gmail.com>
2026-03-09 21:09:40 +0530

    auth controller.js & setOtp controller

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,33 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js"
+import { ApiResponse } from "../utils/ApiResponse.js"
+import { User } from "../models/user.model.js"
+
+const sendOTP = asyncHandler(async(req, res)=>{
+    const { email, mobile} = req.body;
+
+    if(!email && !mobile){
+        throw new ApiError(400, "Email or Mobile number is required")
+    }
+
+    const otp = Math.floor(100000 + Math.random() * 900000).toString();
+
+    let user = await User.findOne({
+        $or: [{email}, {mobile}]
+    })
+
+     if (!user) {
+      user = new User({ email, mobile });
+    }
+
+    user.otp = otp;
+    user.otpExpiry = Date.now() + 5 * 60 * 1000;
+
+    await user.save();
+
+    console.log("Generated OTP:", otp);
+
+    return res.status(200).json(
+        new ApiResponse(200, "OTP sent successfully")
+    )
+})
